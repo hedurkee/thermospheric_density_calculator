9efde25f62cb87d0ac61d2c82db2311f95952fd3
Kendra Swanson <2436551S@student.gla.ac.uk>
2019-11-22 11:08:40 -0500

    Update expected string formatting

--- data_reduction.py
+++ data_reduction.py
@@ -1,19 +1,19 @@
 class DataPoint:
-    def __init__(self, x, y, z, t):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.t = t
+    def __init__(self, lat, long, alt, time):
+        self.lat = lat
+        self.long = long
+        self.alt = alt
+        self.time = time
 
     def __init__(self):
-        self.x=0
-        self.y=0
-        self.z=0
-        self.t=0
+        self.lat=0.0
+        self.long=0.0
+        self.alt=0.0
+        self.time=0.0
 
     def __str__(self):
-        return "DataPoint(x="+str(self.x)+",y="+str(self.y)+",z="+\
-               str(self.z)+",t="+str(self.t)+")"
+        return "DataPoint(latitude="+str(self.lat)+",longitude="+str(self.long)+",altitude="+\
+               str(self.alt)+",time="+str(self.time)+")"
 
 # Constants:
 MASS_EARTH = 5.972 * (10**24) # kg
@@ -26,7 +26,7 @@ DRAG_COEF = 1.8
 Computes the velocity at the midpoint of p1 and p2 given two DataPoints
 '''
 def getVelocity(p1, p2):
-    return (p2.z - p1.z)/(p2.t - p1.t)  # Assumes z is in meters and t is in seconds
+    return (p2.alt - p1.alt)/(p2.time - p1.time)  # Assumes alt is in meters and time is in seconds
 
 
 '''
@@ -51,15 +51,12 @@ Computes the density at a given point using its predecessor and successor
 def density(p, prev, next):
     a = getAcceleration(prev, p, next)
     v = getVelocity3(prev, p, next)
-    return ((2*MASS_SATELLITE*a) + (2*GRAV_CONST*MASS_EARTH*MASS_SATELLITE)/(p.z + RADIUS_EARTH))/(DRAG_COEF*v*v)
+    return ((2*MASS_SATELLITE*a) + (2*GRAV_CONST*MASS_EARTH*MASS_SATELLITE)/(p.alt + RADIUS_EARTH))/(DRAG_COEF*v*v)
 
 '''
 Converts incoming data as a string to an array of objects of type DataPoint.
-Expected string format: "x=_,y=_,z=_,t=_,"
-
-Updated sample input: "123519,4807.038,N,01131.000,E,545.4" 
+Expected string format: "123519,4807.038,N,01131.000,E,545.4" 
 representing "UTCtime,latitude,longitude,altitude"
-(code not yet updated to expect this input)
 '''
 def parse_data_string(s):
     output = []
@@ -67,39 +64,27 @@ def parse_data_string(s):
     the_var = "x"
     the_number = ""
 
-    for i in range(len(s)):
-        current_char = s[i]
-        
-        if current_char == ',':
-            ## convert the string we're working on into a number value
-            ## assign to x, y, z, or t
-            if the_var == "x":
-                the_point.x = float(the_number)
-                the_number=""
-            elif the_var == "y":
-                the_point.y = float(the_number)
-                the_number=""
-            elif the_var == "z":
-                the_point.z = float(the_number)
-                the_number=""
-            elif the_var == "t":
-                the_point.t = float(the_number)
-                the_number=""
-                output.append(the_point)
-                the_point = DataPoint()
-        elif current_char == "x" or current_char == "y" or \
-             current_char == "z" or current_char == "t":
-            the_var = current_char
-        elif current_char == "=":
-            1
-        else:
-            the_number += current_char
+    a = s.split(',')
+
+    for i in range(int(len(a) / 6)):
+        # index 0 = UTC time
+        the_point.time = int(a.pop(0))
+        # index 1 = lat
+        the_point.lat = float(a.pop(0))
+        a.pop(0)
+        # index 3 = long
+        the_point.long = float(a.pop(0))
+        a.pop(0)
+        # index 5 = alt
+        the_point.alt = float(a.pop(0))
+        output.append(the_point)
+        the_point = DataPoint()
     return output
 
 def main():
 
     # test data
-    s = "x=0,y=0,z=100000,t=123519,x=0,y=0,z=99500,t=123550,x=0,y=0,z=99000,t=123585,"
+    s = "123519,4807.038,N,01131.000,E,545.4,123533,4807.038,N,01131.000,E,540.4,123553,4807.038,N,01131.000,E,532.4,"
 
     output = parse_data_string(s)
 
